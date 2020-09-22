af4a3ce483738a755abfd6decadceaca18b4ed1d
Allison Ricker <alliricker@gmail.com>
2020-09-22 16:33:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Aldo", "Bean", "Ezra", "Coogan"]
-  array << element
+  dogs = ["Aldo", "Bean", "Ezra", "Coogan"]
+  dogs.unshift("Bruno")
 end
 
 def add_element_to_start_of_array(array, element)
